b4ba3ec67e1e78aa3579f56f0e7280d7345e62ba
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-02-06 04:45:26 -0500

    Initial Commit

---
+++ SEPTA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SEPTA
+//
+//  Created by Julia Miller on 2/6/17.
+//  Copyright © 2017 Julia Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
