331dbed023e5b4ddc755b082fcafa691aa98c405
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-02-06 05:38:04 -0500

    Set up view.

---
+++ SEPTA/SchedulesVC.swift
@@ -0,0 +1,19 @@
+//
+//  SchedulesVC.swift
+//  SEPTA
+//
+//  Created by Julia Miller on 2/6/17.
+//  Copyright © 2017 Julia Miller. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SchedulesVC: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        //
+    }
+}
---
+++ Schedules.swift
@@ -0,0 +1,9 @@
+//
+//  Schedules.swift
+//  SEPTA
+//
+//  Created by Julia Miller on 2/6/17.
+//  Copyright © 2017 Julia Miller. All rights reserved.
+//
+
+import Foundation
